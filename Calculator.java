57da016aa0312b0d13ffeb3acea1d324ef3442e2
niya2025 <niyasuresh101@gmail.com>
2026-03-18 15:27:43 +0530

    completed

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+package mathutils;
+
+public class Calculator{
+ public double add(double a,double b){
+ return a + b;
+ }
+ public double sub(double a,double b){
+ return a - b;
+ }
+ public double mult(double a, double b){
+ return a * b;
+ }
+ public double div(double a,double b){
+ 
+ return (double) a / b;
+ 
+ }
+ }
